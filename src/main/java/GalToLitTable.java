749efdabaebdc3a1dc16fed7b06066348fd3e2ac
hristoliubova <hristoluybova13@gmail.com>
2022-09-10 17:43:43 +0300

    Implement class and method that convert Gallons to Liters

---
+++ src/main/java/GalToLitTable.java
@@ -0,0 +1,8 @@
+public class GalToLitTable {
+    public static void printGal() {
+        double gallons, litr;
+        gallons = 10;
+        litr = gallons * 3.7854;
+        System.out.println(gallons + " галлонам соответствует " + litr + " литров");
+    }
+}
